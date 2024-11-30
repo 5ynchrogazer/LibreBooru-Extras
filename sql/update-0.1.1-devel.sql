964bf8ee1d23fb8691783d24006a85e5d0fb6681
Aether <selim.y.m.b@web.de>
2024-11-30 09:34:07 +0100

    Updated

---
+++ sql/update-0.1.1-devel.sql
@@ -0,0 +1,4 @@
+-- Adding a parent_id column to the posts table for parent and child posts
+ALTER TABLE posts
+ADD COLUMN parent_id INT, -- ID of the parent post
+ADD CONSTRAINT fk_parent_id FOREIGN KEY (parent_id) REFERENCES posts (post_id) ON DELETE SET NULL;
