964c777c3f4b980b57ff4af828fdbb6eaeabf4a6
Aether <selim.y.m.b@web.de>
2024-12-02 15:47:19 +0100

    let's just hope that it doesnt exist already~

--- sql/update-0.1.1-devel.sql
+++ sql/update-0.1.1-devel.sql
@@ -1,9 +1,4 @@
-SELECT COUNT(*) AS column_exists
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'posts' AND COLUMN_NAME = 'parent_id';
-
+-- Adding a parent_id column to the posts table for parent and child posts
 ALTER TABLE posts
-ADD COLUMN IF NOT EXISTS parent_id INT;
-
-ALTER TABLE posts
-ADD CONSTRAINT IF NOT EXISTS fk_parent_id FOREIGN KEY (parent_id) REFERENCES posts(id) ON DELETE SET NULL;
+ADD COLUMN parent_id INT, -- ID of the parent post
+ADD CONSTRAINT fk_parent_id FOREIGN KEY (parent_id) REFERENCES posts (post_id) ON DELETE SET NULL;
