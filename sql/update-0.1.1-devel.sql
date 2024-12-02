250127e7877540e7b3cc1cc1470d52e333d33047
Aether <selim.y.m.b@web.de>
2024-12-02 15:20:21 +0100

    Update update-0.1.1-devel.sql

--- sql/update-0.1.1-devel.sql
+++ sql/update-0.1.1-devel.sql
@@ -1,4 +1,13 @@
--- Adding a parent_id column to the posts table for parent and child posts
-ALTER TABLE posts
-ADD COLUMN parent_id INT, -- ID of the parent post
-ADD CONSTRAINT fk_parent_id FOREIGN KEY (parent_id) REFERENCES posts (post_id) ON DELETE SET NULL;
+-- Check if parent_id column exists
+SET @column_exists = (
+    SELECT COUNT(*)
+    FROM INFORMATION_SCHEMA.COLUMNS
+    WHERE TABLE_NAME = 'posts' AND COLUMN_NAME = 'parent_id'
+);
+
+-- Add column and constraint if they don't exist
+IF @column_exists = 0 THEN
+    ALTER TABLE posts
+    ADD COLUMN parent_id INT, -- ID of the parent post
+    ADD CONSTRAINT fk_parent_id FOREIGN KEY (parent_id) REFERENCES posts(id) ON DELETE SET NULL;
+END IF;
