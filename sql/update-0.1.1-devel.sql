a32d03350e918fef026c55f6c7375e163fcb11d7
Aether <selim.y.m.b@web.de>
2024-12-02 15:51:38 +0100

    Update update-0.1.1-devel.sql

--- sql/update-0.1.1-devel.sql
+++ sql/update-0.1.1-devel.sql
@@ -2,3 +2,6 @@
 ALTER TABLE posts
 ADD COLUMN parent_id INT, -- ID of the parent post
 ADD CONSTRAINT fk_parent_id FOREIGN KEY (parent_id) REFERENCES posts (post_id) ON DELETE SET NULL;
+
+ALTER TABLE favourites
+ADD COLUMN test INT;
